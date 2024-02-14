bff04bb1414237cb1c120b65a71bb87200d9d13e
Diego Armando Zambrana Arze <contacto@diegozambrana.com>
2024-02-14 10:24:49 -0400

    develop

--- next.config.mjs
+++ next.config.mjs
@@ -6,21 +6,21 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
+            ? "http://api-dz.com/api/:path*"
             : "/api/",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
+            ? "http://api-dz.com/docs"
             : "/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
+            ? "http://api-dz.com/openapi.json"
             : "/api/openapi.json",
       },
     ];
