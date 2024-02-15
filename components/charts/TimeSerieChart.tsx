5a9ce18984046e1afcacce82aa874a991557ee96
Diego Zambrana <contacto@diegozambrana.com>
2024-02-15 17:11:58 -0400

    Chart updates (#26) (#27)
    * added avatar, created dashboardElement, added download link (#25)
    
    * updated line chart, added legends

--- components/charts/TimeSerieChart.tsx
+++ components/charts/TimeSerieChart.tsx
@@ -9,6 +9,12 @@ import {
 } from '@visx/xychart';
 import { FC } from 'react';
 
+import { LegendOrdinal, LegendThreshold } from '@visx/legend';
+import { scaleOrdinal, scaleThreshold } from '@visx/scale';
+import { Loading } from '../ui/Loading';
+import { COLORS, COLORS_FORECAST } from '@/utils/constants';
+
+
 interface TimeSerieChartType{
   series: {
     name: string;
@@ -27,49 +33,72 @@ export const TimeSerieChart: FC<TimeSerieChartType> = ({series, predictions}) =>
   };
 
   if (!series && !predictions) {
-    return <div>...</div>;
+    return <Loading height='400px' />;
   }
+
+  const ordinalColorScale = scaleOrdinal({
+    domain: series.map((s) => s['name']),
+    range: COLORS.slice(0, series.length),
+  });
+  
 
   // TODO: Add a loading state and legends
   return (
-    <XYChart height={450} xScale={{ type: "time" }} yScale={{ type: "linear" }}>
-      <AnimatedAxis orientation="bottom" numTicks={4} />
-      <AnimatedAxis orientation="left" numTicks={4} />
-      <AnimatedGrid columns={false} numTicks={4} />
-      {series.map((serie, index) => (
-        <AnimatedLineSeries
-          key={serie.name}
-          dataKey={serie.name}
-          data={serie.data}
-          {...accessors}
-        />
-      ))}
-      {predictions.map((prediction, index) => (
-        <AnimatedLineSeries
-          key={`p_${prediction.name}`}
-          dataKey={`p_${prediction.name}`}
-          data={prediction.data}
-          {...accessors}
-        />
-      ))}
-      <Tooltip
-        snapTooltipToDatumX
-        snapTooltipToDatumY
-        renderTooltip={({ tooltipData, colorScale }) => {
-          if(tooltipData?.nearestDatum === undefined || !colorScale) return <></>
-          return (
-            <div>
-              <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
-                {tooltipData.nearestDatum.key}
+    <div style={{position: 'relative'}}>
+      <XYChart height={400} xScale={{ type: "time" }} yScale={{ type: "linear" }}>
+        <AnimatedAxis orientation="bottom" numTicks={4} />
+        <AnimatedAxis orientation="left" numTicks={4} />
+        <AnimatedGrid columns={false} numTicks={4} />
+        {series.map((serie, index) => (
+          <AnimatedLineSeries
+            key={serie.name}
+            dataKey={serie.name}
+            data={serie.data}
+            stroke={COLORS[index]}
+            {...accessors}
+          />
+        ))}
+        {predictions.map((prediction, index) => (
+          <AnimatedLineSeries
+            key={`p_${prediction.name}`}
+            dataKey={`p_${prediction.name}`}
+            data={prediction.data}
+            stroke={COLORS_FORECAST[index]}
+            {...accessors}
+          />
+        ))}
+        <Tooltip
+          snapTooltipToDatumX
+          snapTooltipToDatumY
+          renderTooltip={({ tooltipData, colorScale }) => {
+            if(tooltipData?.nearestDatum === undefined || !colorScale) return <></>
+            return (
+              <div>
+                <div>
+                  {tooltipData.nearestDatum.key}
+                </div>
+                {accessors.xAccessor(tooltipData.nearestDatum.datum)?.toISOString().split('T')[0]}
+                {", "}
+                {accessors.yAccessor(tooltipData.nearestDatum.datum)}
               </div>
-              {accessors.xAccessor(tooltipData.nearestDatum.datum)?.toISOString().split('T')[0]}
-              {", "}
-              {accessors.yAccessor(tooltipData.nearestDatum.datum)}
-            </div>
-          )
-        }}
-      />
-
-    </XYChart>
+            )
+          }}
+        />
+        
+      </XYChart>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: '4rem',
+      }}>
+        <LegendOrdinal
+          scale={ordinalColorScale}
+          direction="column-reverse"
+          itemDirection="row-reverse"
+          labelMargin="0 20px 0 0"
+          shapeMargin="1px 0 0"
+        />
+      </div>
+    </div>
   )
 }
